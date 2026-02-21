f265ca8c6d3ada279c6ddd5623eeca1822d536d5
wwoozzii <gmar4630@gmail.com>
2026-02-21 19:28:18 +0300

    feat: initial project setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,18 @@ function App() {
   const [tasks, setTasks] = useState([]);
   const [inputTasks, setInputTask] = useState("");
 
-  const [editingId, setEditingId] = useState(null); //запоминаю кого (что) редактирую, (id)
-	const [editingvalue, setEditingValue] = useState(""); // запоминаю текст из редактора для вставки в (name)
-	// начало: функция в которую передаю id и name, вызов при клике на изменить
+  const [editingId, setEditingId] = useState(null);
+  const [editingvalue, setEditingValue] = useState("");
 
-
-
+  const [category, setCategory] = useState("all"); // 'all', 'active', 'complited'
 
   function addTaskBtn() {
     if (inputTasks === "") return;
 
-    setTasks([{ id: Date.now(), name: inputTasks }, ...tasks]);
+    setTasks((prev) => [
+      { id: Date.now(), name: inputTasks, complited: false },
+      ...prev,
+    ]);
     setInputTask("");
   }
 
@@ -23,17 +24,105 @@ function App() {
     setTasks(tasks.filter((massiv) => massiv.id !== id));
   }
 
+  function startEditTask(id, name) {
+    setEditingId(id);
+    setEditingValue(name);
+    console.log("start edit:", id, name);
+  }
+
+  function saveEdit(id) {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, name: editingvalue } : task,
+      ),
+    );
+    setEditingValue("");
+    setEditingId(null);
+  }
+
+  const filteredTasks = tasks.filter((task) => {
+    if (category === "active") return !task.complited;
+    if (category === "complited") return task.complited;
+    return true; //all
+  });
+
+  function toggleComplited(id) {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, complited: !task.complited } : task,
+      ),
+    );
+    console.log("complited");
+  }
+
   return (
     <>
+      {/*кнопки фильтрации */}
+      <button
+        onClick={() => {
+          setCategory("all");
+        }}
+      >
+        All
+      </button>
+      <button
+        onClick={() => {
+          setCategory("active");
+        }}
+      >
+        Active
+      </button>
+      <button
+        onClick={() => {
+          setCategory("complited");
+        }}
+      >
+        Complited
+      </button>
+
+      {/*добавление задачи */}
       <input
         type="text"
         value={inputTasks}
         onChange={(e) => setInputTask(e.target.value)}
       ></input>
       <button onClick={addTaskBtn}>add</button>
-      {tasks.map((massiv) => (
+
+      {filteredTasks.map((massiv) => (
         <div key={massiv.id}>
-          {massiv.name}
+          {editingId === massiv.id ? (
+            <>
+              <button
+                onClick={() => {
+                  saveEdit(massiv.id);
+                }}
+              >
+                save
+              </button>
+
+              <input
+                type="text"
+                value={editingvalue}
+                onChange={(e) => setEditingValue(e.target.value)}
+              ></input>
+            </>
+          ) : (
+            massiv.name
+          )}
+          <button
+            onClick={() => {
+              startEditTask(massiv.id, massiv.name);
+            }}
+          >
+            edit
+          </button>
+          <button
+            onClick={() => {
+              toggleComplited(massiv.id);
+            }}
+          >
+            complited
+          </button>
           <button
             onClick={() => {
               deleteTaskBtn(massiv.id);
