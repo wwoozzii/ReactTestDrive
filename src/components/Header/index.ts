deb35da74c22b4e843234b627129d8de1bf20e0c
wwoozzii <gmar4630@gmail.com>
2026-03-31 20:51:51 +0300

    feat: mobile test version

---
+++ src/components/Header/index.ts
@@ -0,0 +1,3 @@
+// src/components/Header/index.js
+export { Header } from "./Header.js";
+
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 // src/components/index.ts
+export { EditMode } from "./EditMode/index.js";
 export { TaskCount } from "./TaskCount/index.js";
 export { TaskFilter } from "./TaskFilter/index.js";
 export { TaskInput } from "./TaskInput/index.js";
@@ -6,4 +7,6 @@ export { TaskItem } from "./TaskItem/index.js";
 export { TaskList } from "./TaskList/index.js";
 export { TaskLoadButton } from "./TaskLoad/index.js";
 export { TaskSearch } from "./TaskSearch/index.js";
+
+export { Header } from "./Header/index.js";
 
