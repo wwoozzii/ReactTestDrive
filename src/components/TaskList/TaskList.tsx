c5c15b61b6c5331955c0989860d519a21bbd3bcf
wwoozzii <gmar4630@gmail.com>
2026-04-30 21:28:38 +0300

    fix(TaskList): delete unused import func

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -4,19 +4,13 @@ import { TaskItem } from "../TaskItem/TaskItem.js";
 import s from "./TaskList.module.scss";
 
 export function TaskList() {
-  const { filteredTasks, onDelete, onToggle, onSave } = useTasks();
+  const { filteredTasks } = useTasks();
   const safeTasks = filteredTasks || [];
 
   return (
     <div className={s.listContainer}>
       {safeTasks.filter(Boolean).map((t: Task) => (
-        <TaskItem
-          key={t.id}
-          task={t}
-          onDelete={onDelete}
-          onToggle={onToggle}
-          onSave={onSave}
-        />
+        <TaskItem key={t.id} task={t} />
       ))}
     </div>
   );
